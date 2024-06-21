79d45e47213e4aca51e32b0dff3e72975e6c272c
Stephen Alexander Duran <criaric@ymail.com>
2024-06-21 16:47:45 -0500

    Refactored

--- src/main/java/com/PoeDropNotifs/POEDropNotifConfig.java
+++ src/main/java/com/PoeDropNotifs/POEDropNotifConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.PoeDropNotifs;
 
 import net.runelite.api.ItemID;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
+@ConfigGroup("PoeDropNotifs")
 public interface POEDropNotifConfig extends Config
 {
 	// @ConfigItem(
--- src/main/java/com/PoeDropNotifs/POEDropNotifPlugin.java
+++ src/main/java/com/PoeDropNotifs/POEDropNotifPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.PoeDropNotifs;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
